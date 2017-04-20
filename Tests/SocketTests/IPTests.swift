21909db6aef60d1a48b85dd8a8427e34053e0faa
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-04-20 11:04:09 +0000

    Add IP structs, socket address family

---
+++ Tests/SocketTests/IPTests.swift
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2017 Tris Foundation and the project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License
+ *
+ * See LICENSE.txt in the project root for license information
+ * See CONTRIBUTORS.txt for the list of the project authors
+ */
+
+import Dispatch
+@testable import Socket
+
+class IPTests: TestCase {
+    func testIPv4() {
+        let ip4 = IPv4(127,0,0,1)
+        assertNotNil(ip4)
+    }
+
+    func testIPv6() {
+        let ip6 = IPv6(0,0,0,0,0,0,0,1)
+        assertNotNil(ip6)
+    }
+
+
+    static var allTests = [
+        ("testIPv4", testIPv4),
+        ("testIPv6", testIPv6),
+    ]
+}
