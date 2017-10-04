f04b91447a196e2208b468f9f4568131e68653d7
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-10-04 05:19:02 +0000

    [Socket.Options] Fix visibility

--- Sources/Network/Socket/Socket+Options.swift
+++ Sources/Network/Socket/Socket+Options.swift
@@ -26,11 +26,11 @@ extension Socket {
             self.descriptor = descriptor
         }
 
-        func get(_ option: Option) throws -> Bool {
+        public func get(_ option: Option) throws -> Bool {
             return try getValue(for: option.rawValue)
         }
 
-        func set(_ option: Option, _ value: Bool) throws {
+        public func set(_ option: Option, _ value: Bool) throws {
             try setValue(value, for: option.rawValue)
         }
 
