f40b27e34e174c19f3652086d68edfc64004467e
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-04-04 23:08:33 +0000

    [File] Restore convenience initializer from string

--- Sources/File/File.swift
+++ Sources/File/File.swift
@@ -137,6 +137,11 @@ extension File {
         }
         self.init(name: name, at: path)
     }
+
+    convenience
+    public init(string: String) throws {
+        try self.init(path: Path(string: string))
+    }
 }
 
 // MARK: description
