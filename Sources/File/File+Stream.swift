f5b4666463f91e94d40782e99f12f74f13f80d28
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-04-22 18:04:22 +0000

    Unlicense

--- Sources/File/File+Stream.swift
+++ Sources/File/File+Stream.swift
@@ -1,3 +1,16 @@
+/******************************************************************************
+ *                                                                            *
+ * Tris Foundation disclaims copyright to this source code.                   *
+ * In place of a legal notice, here is a blessing:                            *
+ *                                                                            *
+ *     May you do good and not evil.                                          *
+ *     May you find forgiveness for yourself and forgive others.              *
+ *     May you share freely, never taking more than you give.                 *
+ *                                                                            *
+ ******************************************************************************
+ *  This file contains code that has not yet been described                   *
+ ******************************************************************************/
+
 import Stream
 import Platform
 
