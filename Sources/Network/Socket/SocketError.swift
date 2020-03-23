dda031a60974b05e007b83833a7f172c753b1a2e
Tony Freeman <tonyfreeman@users.noreply.github.com>
2020-03-23 16:55:50 +0000

    Add temporary fix for SystemError

--- Sources/Network/Socket/SocketError.swift
+++ Sources/Network/Socket/SocketError.swift
@@ -1,10 +1,22 @@
 import Platform
 
-public struct SocketError: Error, CustomStringConvertible {
-    public let number = errno
-    public let description = String(cString: strerror(errno))
+public struct SocketError: Error, Equatable, CustomStringConvertible {
+    public let number: Int32
+    public var description: String { .init(cString: strerror(errno)) }
+
+    public init(number: Int32 = errno) {
+        self.number = number
+    }
 
     public var interrupted: Bool {
         return number == EAGAIN || number == EWOULDBLOCK || number == EINTR
+    }
+
+    public static var badDescriptor: SocketError {
+        return SocketError(number: EBADF)
+    }
+
+    public static var invalidArgument: SocketError {
+        return SocketError(number: EINVAL)
     }
 }
